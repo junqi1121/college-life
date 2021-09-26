043b330a7349e0e3d099addaa78a6c2620a24b3a
junqi1121 <90767596+junqi1121@users.noreply.github.com>
2021-09-26 15:14:39 +0800

    Update 一元二次方程组求解.c

--- 一元二次方程组求解.c
+++ 一元二次方程组求解.c
@@ -3,11 +3,12 @@
 int main()
 {
 	float a=0, b=0, c=0, derta=0;
-	printf("请输入方程的二次项系数：\n");
+	printf("请将方程整理成标准形式ax^2+bx+c=0\n");
+	printf("请输入方程的二次项系数a：\n");
 	scanf("%f",&a);
-	printf("请输入方程的一次项系数：\n");
+	printf("请输入方程的一次项系数b：\n");
 	scanf("%f",&b);
-	printf("请输入方程的常数项：\n");
+	printf("请输入方程的常数项c：\n");
 	scanf("%f",&c);
 	derta = b * b - 4 * a * c;
 	if (derta < 0)
